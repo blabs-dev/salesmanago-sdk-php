51be875eef0cb2c2c156c2a3383f4a0c10c62ff6
Salvo Bonanno <salvob.labs@gmail.com>
2022-12-30 14:19:24 +0100

    Configurator Test

---
+++ tests/ConfiguratorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Blabs\SalesManago\Test;
+
+use Blabs\SalesManago\Configurator;
+use PHPUnit\Framework\TestCase;
+
+class ConfiguratorTest extends TestCase
+{
+    private string $testClientId;
+    private string $testApiKey;
+    private string $testApiSecret;
+
+    /**
+     * @test
+     */
+    public function it_can_generate_configuration_data_properly()
+    {
+
+        $configurator = new Configurator(
+            $this->testClientId,
+            $this->testApiKey,
+            $this->testApiSecret
+        );
+
+        $this->assertEquals(
+            sha1($this->testApiKey.$this->testClientId.$this->testApiSecret),
+            $configurator->getSha()
+        );
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->testClientId = 'test-client-id';
+        $this->testApiKey = 'test-api-key';
+        $this->testApiSecret = 'test-api-secret';
+    }
+}
