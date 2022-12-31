3a342818ed6473cf90e015192b0d023bbff6406b
Salvo Bonanno <salvob.labs@gmail.com>
2022-12-31 07:27:34 +0100

    Contact Service factory test

--- tests/ServiceFactoryTest.php
+++ tests/ServiceFactoryTest.php
@@ -2,8 +2,21 @@
 
 namespace Blabs\SalesManago\Test;
 
+use Blabs\SalesManago\Configurator;
+use Blabs\SalesManago\ServiceFactory;
+use Blabs\SalesManago\Services\ContactService;
 use PHPUnit\Framework\TestCase;
 
 class ServiceFactoryTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function it_can_create_contact_service()
+    {
+        $configurator = new Configurator('test-client-id', 'test-api-secret');
+        $factory = new ServiceFactory($configurator);
+        $service = $factory->createContactService();
+        $this->assertEquals(ContactService::class, get_class($service));
+    }
 }
